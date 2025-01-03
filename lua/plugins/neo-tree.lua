c591aabd4df663c68974ec3c6915ab10035f2afc
Sean Daly <the_sean_c@fastmail.com>
2025-01-02 21:33:53 -0500

    added left/right keys to open files from neo-tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+{
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["<right>"] = "open",
+        ["<left>"] = "close_node",
+      },
+    },
+  },
+},
+}
